7fc96742de1d76e76a406be94d26ebf7fdaa6ac9
Bidhan Baral <bbaral2062@gmail.com>
2025-02-16 12:47:31 -0500

    Finished with most of the app, however I still need to implement the logic to show the added book in the library

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 const library = [];
+const dialog = document.querySelector("#dialog");
+const openDialog = document.querySelector("#add_book");
+const closeDialog = document.querySelector("#close");
+const dialogForm = document.querySelector("#form");
+const readStatus = document.querySelector("#status");
+
+readStatus.addEventListener("click", () => {
+    if (readStatus.textContent == "Unread") {
+        readStatus.textContent = "Read";
+        readStatus.style.backgroundColor = "rgb(94, 240, 106)";
+    } else {
+        readStatus.textContent = "Unread";
+        readStatus.style.backgroundColor = "rgb(207, 207, 207)";
+    }
+})
+
+openDialog.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+closeDialog.addEventListener("click", () => {
+    dialog.close();
+    dialogForm.reset();
+});
+
+function book() {
+    
+}
+
+function addBook() {
+
+}
