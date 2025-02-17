7be906b2b80fc39096ec7a3cd6d3f35a9fc45d9f
Bidhan Baral <bbaral2062@gmail.com>
2025-02-17 18:54:03 -0500

    Completed the javascript logic for adding a new book. Now just have to create elements in javascript and display them accordingly

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const openDialog = document.querySelector("#add_book");
 const closeDialog = document.querySelector("#close");
 const dialogForm = document.querySelector("#form");
 const readStatus = document.querySelector("#status");
+const addButton = document.querySelector("#add");
 
 readStatus.addEventListener("click", () => {
     if (readStatus.textContent == "Unread") {
@@ -24,10 +25,42 @@ closeDialog.addEventListener("click", () => {
     dialogForm.reset();
 });
 
-function book() {
+addButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const bookTitle = document.querySelector("#new-book-title").value;
+    const bookAuthor = document.querySelector("#new-book-author").value;
+    const bookPages = document.querySelector("#new-book-pages").value;
+    const bookRating = document.querySelector("#new-book-rating").value;
+    const bookStatus = "Unread";
     
+    if (!bookTitle || !bookAuthor) {
+        alert("Please input information for title and author");
+        return;
+    } else if(!bookPages || Number(bookPages) < 0 || Number(bookPages) > 10000) {
+        alert("Please input a number between 0-10000 for pages");
+        return;
+    } else if(!bookRating || Number(bookRating) < 0 || Number(bookPages) > 5) {
+        alert("Please input a number between 0-5 for rating");
+        return;
+    }
+
+    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRating);
+    dialog.close();
+    dialogForm.reset();
+});
+
+function Book(title, author, pages, rating) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.rating = rating;
 }
 
-function addBook() {
+function addBookToLibrary(title, author, pages, rating) {
+    const newBook = new Book(title, author, pages, rating);
+    library.push(newBook);
+    console.log(library[0]);
+
 
 }
+
